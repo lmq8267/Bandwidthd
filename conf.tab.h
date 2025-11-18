e2dc6d1342829e94ff67be93fa50c210fec335ed
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2025-11-18 17:02:06 +0800

    Update conf.tab.h

--- conf.tab.h
+++ conf.tab.h
@@ -1,12 +1,14 @@
-/* A Bison parser, made by GNU Bison 1.875.  */
+/* A Bison parser, made by GNU Bison 3.8.2.  */
 
-/* Skeleton parser for Yacc-like parsing with Bison,
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002 Free Software Foundation, Inc.
+/* Bison interface for Yacc-like parsers in C
 
-   This program is free software; you can redistribute it and/or modify
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,79 +16,90 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
-/* As a special exception, when this file is copied by Bison into a
-   Bison output file, you may use that output file without restriction.
-   This special exception was added by the Free Software Foundation
-   in version 1.24 of Bison.  */
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
 
-/* Tokens.  */
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_BDCONFIG_CONF_TAB_H_INCLUDED
+# define YY_BDCONFIG_CONF_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int bdconfig_debug;
+#endif
+
+/* Token kinds.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     TOKJUNK = 258,
-     TOKSUBNET = 259,
-     TOKDEV = 260,
-     TOKSLASH = 261,
-     TOKSKIPINTERVALS = 262,
-     TOKGRAPHCUTOFF = 263,
-     TOKPROMISC = 264,
-     TOKOUTPUTCDF = 265,
-     TOKRECOVERCDF = 266,
-     TOKGRAPH = 267,
-     TOKNEWLINE = 268,
-     TOKFILTER = 269,
-     TOKMETAREFRESH = 270,
-     TOKPGSQLCONNECTSTRING = 271,
-     TOKSENSORID = 272,
-     IPADDR = 273,
-     NUMBER = 274,
-     STRING = 275,
-     STATE = 276
-   };
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    TOKJUNK = 258,                 /* TOKJUNK  */
+    TOKSUBNET = 259,               /* TOKSUBNET  */
+    TOKDEV = 260,                  /* TOKDEV  */
+    TOKSLASH = 261,                /* TOKSLASH  */
+    TOKSKIPINTERVALS = 262,        /* TOKSKIPINTERVALS  */
+    TOKGRAPHCUTOFF = 263,          /* TOKGRAPHCUTOFF  */
+    TOKPROMISC = 264,              /* TOKPROMISC  */
+    TOKOUTPUTCDF = 265,            /* TOKOUTPUTCDF  */
+    TOKRECOVERCDF = 266,           /* TOKRECOVERCDF  */
+    TOKGRAPH = 267,                /* TOKGRAPH  */
+    TOKNEWLINE = 268,              /* TOKNEWLINE  */
+    TOKFILTER = 269,               /* TOKFILTER  */
+    TOKMETAREFRESH = 270,          /* TOKMETAREFRESH  */
+    TOKPGSQLCONNECTSTRING = 271,   /* TOKPGSQLCONNECTSTRING  */
+    TOKSENSORID = 272,             /* TOKSENSORID  */
+    TOKSQLITEFILENAME = 273,       /* TOKSQLITEFILENAME  */
+    IPADDR = 274,                  /* IPADDR  */
+    NUMBER = 275,                  /* NUMBER  */
+    STRING = 276,                  /* STRING  */
+    STATE = 277                    /* STATE  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
 #endif
-#define TOKJUNK 258
-#define TOKSUBNET 259
-#define TOKDEV 260
-#define TOKSLASH 261
-#define TOKSKIPINTERVALS 262
-#define TOKGRAPHCUTOFF 263
-#define TOKPROMISC 264
-#define TOKOUTPUTCDF 265
-#define TOKRECOVERCDF 266
-#define TOKGRAPH 267
-#define TOKNEWLINE 268
-#define TOKFILTER 269
-#define TOKMETAREFRESH 270
-#define TOKPGSQLCONNECTSTRING 271
-#define TOKSENSORID 272
-#define IPADDR 273
-#define NUMBER 274
-#define STRING 275
-#define STATE 276
 
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 39 "conf.y"
 
-
-
-#if ! defined (YYSTYPE) && ! defined (YYSTYPE_IS_DECLARED)
-#line 38 "conf.y"
-typedef union YYSTYPE {
     int number;
     char *string;
-} YYSTYPE;
-/* Line 1248 of yacc.c.  */
-#line 83 "y.tab.h"
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
+
+#line 91 "conf.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE bdconfig_lval;
 
 
+int bdconfig_parse (void);
 
+
+#endif /* !YY_BDCONFIG_CONF_TAB_H_INCLUDED  */
